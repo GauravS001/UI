ea43cbaab14e5b0bc99e913ec1e5cdabd7557e8a
GauravS001 <130973373+GauravS001@users.noreply.github.com>
2023-04-23 08:48:16 +0530

    Add files via upload

--- JoySlider.js
+++ JoySlider.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { ScrollView, View, FlatList, Text, TouchableOpacity, Image} from "react-native";
-import {LinearGradient} from 'expo-linear-gradient';
 import JoySliderComps from "./JoySliderComps";
+
 
 
   
@@ -14,22 +14,22 @@ export default function JoySlider(){
         
         <View style={{width: "100%", height: 305, backgroundColor: "white"}}>
 
-            <Text style={{fontSize: 23, position: "absolute", fontWeight: 600, marginLeft: 18, marginTop: 4, width: 200}}>Where will you find joy?</Text>
+<Text style={{fontSize: 23, position: "absolute", fontWeight: 600, marginLeft: 18, marginTop: 4, width: 200, color: "black"}}>Where will you find joy?</Text>
 
-            <TouchableOpacity style={{width: 100, height: 28, backgroundColor: "rgb(253, 81, 18)", position: "absolute", borderRadius: 8, marginTop: 35, marginLeft: 250}}>
-                <Text style={{fontSize: 13, color: "white", fontWeight: 600, marginLeft: 13, marginTop: 3.5}}>See more</Text>
-                <Image source={require('./assets/forward_50px.png')} style={{position: "absolute", marginLeft: 78, width: 12, height: 12, marginTop: 7.5}}></Image>
-            </TouchableOpacity>
+<TouchableOpacity style={{width: 100, height: 28, backgroundColor: "rgb(253, 81, 18)", position: "absolute", borderRadius: 8, marginTop: 35, marginLeft: 250}}>
+    <Text style={{fontSize: 13, color: "white", fontWeight: 600, marginLeft: 13, marginTop: 3.5}}>See more</Text>
+    <Image source={require('./assets/forward_50px.png')} style={{position: "absolute", marginLeft: 78, width: 12, height: 12, marginTop: 7.5}}></Image>
+</TouchableOpacity>
 
-            <ScrollView horizontal style={{flexDirection: "row"}} showsHorizontalScrollIndicator={false}>
+<ScrollView horizontal style={{flexDirection: "row"}} showsHorizontalScrollIndicator={false}>
+
+<JoySliderComps></JoySliderComps>
+
+</ScrollView>
+
+</View>
+
         
-        
-               <JoySliderComps></JoySliderComps>
-          
-               
-            </ScrollView>
-
-        </View>
 
     );
 
--- JoySliderComps.js
+++ JoySliderComps.js
@@ -1,22 +1,24 @@
 import React from "react";
 import { ScrollView, View, FlatList, Text, TouchableOpacity, Image} from "react-native";
-import {LinearGradient} from 'expo-linear-gradient';
+import LinearGradient from "react-native-linear-gradient";
+
 
 
 var UpperSlide = [];
 var LowerSlide = [];
-var UpperSlideTitle = ['Dubai', 'Bangkok', 'Pattaya', 'Kuala Lumpur', 'Tokyo'];
-var LowerSlideTitle = ['Bali', 'Phuket', 'Paris', 'Da Nang', 'Halong'];
-var UpperSlideBackgrounds = [require('./assets/dubai.webp'), require('./assets/bangkokwebp.webp'), require('./assets/pattaya.webp'), require('./assets/kualalumpur.webp'), require('./assets/tokyo.webp'),];
-var LowerSlideBackgrounds = [require('./assets/bali.webp'),require('./assets/phuket.webp'),require('./assets/pasris.webp'), require('./assets/danang.webp'), require('./assets/halong.webp'),];
+var UpperSlideTitle = ['Varanasi', 'Haridwar', 'Pattaya', 'Kuala Lumpur', 'Tokyo'];
+var LowerSlideTitle = ['Rameshwaram', 'Vaisno  Devi', 'Paris', 'Da Nang', 'Halong'];
+var UpperSlideBackgrounds = [require('./assets/varanasi2.jpg'), require('./assets/Haridwar1.jpg'), require('./assets/pattaya.webp'), require('./assets/kualalumpur.webp'), require('./assets/tokyo.webp'),];
+var LowerSlideBackgrounds = [require('./assets/rameshwaram1.jpg'),require('./assets/vaisno1.webp'),require('./assets/pasris.webp'), require('./assets/danang.webp'), require('./assets/halong.webp'),];
 
 	for(let i = 0; i < 5; i++){
 
 		UpperSlide.push(
 			<View key = {i} style={{width: 160, height: 100, backgroundColor: "green", marginLeft: 13, borderRadius: 17}}>
                 <Image source={UpperSlideBackgrounds[i]} style={{width: 160, height: 100, position: "absolute", borderRadius: 17}}></Image>
-               <LinearGradient colors={['black', 'transparent']} style={{zIndex: 10, width: "100%", height: 70, borderTopLeftRadius: 17, borderTopRightRadius: 17, opacity: 0.58}}></LinearGradient> 
-              <Text style={{color: "white", fontSize: 18.5, fontWeight: 600, position: "absolute", zIndex: 10, marginTop: 10, marginLeft: 15}}>{UpperSlideTitle[i]}</Text>
+
+                <LinearGradient colors={['black', 'transparent']} style={{zIndex: 10, width: "100%", height: 70, borderTopLeftRadius: 17, borderTopRightRadius: 17, opacity: 0.65, position: "absolute"}}></LinearGradient> 
+              <Text style={{color: "white", fontSize: 17.5, fontWeight: 600, position: "absolute", zIndex: 10, marginTop: 8, marginLeft: 15}}>{UpperSlideTitle[i]}</Text>
             </View>
             
 		)
@@ -28,8 +30,8 @@ var LowerSlideBackgrounds = [require('./assets/bali.webp'),require('./assets/phu
 		LowerSlide.push(
 			<View key = {j} style={{width: 160, height: 100, backgroundColor: "green", marginLeft: 13, borderRadius: 17}}>
                  <Image source={LowerSlideBackgrounds[j]} style={{width: 160, height: 100, position: "absolute", borderRadius: 17}}></Image>
-                 <LinearGradient colors={['black', 'transparent']} style={{zIndex: 10, width: "100%", height: 70, borderTopLeftRadius: 17, borderTopRightRadius: 17, opacity: 0.56}}></LinearGradient> 
-                 <Text style={{color: "white", fontSize: 18.5, fontWeight: 600, position: "absolute", zIndex: 10, marginTop: 10, marginLeft: 15}}>{LowerSlideTitle[j]}</Text>
+                 <LinearGradient colors={['black', 'transparent']} style={{zIndex: 10, width: "100%", height: 70, borderTopLeftRadius: 17, borderTopRightRadius: 17, opacity: 0.65, position: "absolute"}}></LinearGradient> 
+                 <Text style={{color: "white", fontSize: 17.5, fontWeight: 600, position: "absolute", zIndex: 10, marginTop: 8, marginLeft: 15}}>{LowerSlideTitle[j]}</Text>
             </View>
 		)
 	}
@@ -47,29 +49,29 @@ export default function JoySliderComps(){
         <View style={{width: 1046, height: "100%", backgroundColor: "transparent"}}>
 
         <View style={{width: "100%", height: 120, backgroundColor: "transparent", position: "absolute", marginTop: 74, flexDirection: "row", marginLeft: 169}}>
-             {UpperSlide}
-         </View>
-
-         <View style={{width: "100%", height: 120, backgroundColor: "transparent", position: "absolute", marginTop: 188, flexDirection: "row", marginLeft: 169}}>
-             {LowerSlide}
-         </View>
-
-         
-
-         <View style={{width: 155, height: 215, backgroundColor: "red", position: "absolute", marginTop: 74, marginLeft: 15, borderRadius: 17}}>
-
-             <Image source={require('./assets/singapore.webp')} style={{width: 155, height: 215, position: "absolute", borderRadius: 17}}></Image>
-
-         <LinearGradient colors={['black', 'transparent']} style={{zIndex: 10, width: "100%", height: 150, borderTopLeftRadius: 17, borderTopRightRadius: 17, opacity: 0.5}}></LinearGradient> 
-              
-              <Text style={{color: "white", fontSize: 18.5, fontWeight: 600, position: "absolute", zIndex: 10, marginTop: 10, marginLeft: 15}}>Singapore</Text> 
-         </View>
-
-         
-        
-        
-
+     {UpperSlide}
         </View>
+
+        <View style={{width: "100%", height: 120, backgroundColor: "transparent", position: "absolute", marginTop: 188, flexDirection: "row", marginLeft: 169}}>
+        {LowerSlide}
+        </View>
+
+ 
+
+ <View style={{width: 155, height: 215, backgroundColor: "red", position: "absolute", marginTop: 74, marginLeft: 15, borderRadius: 17}}>
+
+     <Image source={require('./assets/Tirupati5.jpg')} style={{width: 155, height: 215, position: "absolute", borderRadius: 17}}></Image>
+
+     <LinearGradient colors={['black', 'transparent']} style={{zIndex: 10, width: "100%", height: 150, borderTopLeftRadius: 17, borderTopRightRadius: 17, opacity: 0.45}}></LinearGradient> 
+      
+      <Text style={{color: "white", fontSize: 18, fontWeight: 600, position: "absolute", zIndex: 10, marginTop: 8, marginLeft: 15}}>Tirupati</Text>  
+ </View>
+
+ 
+
+
+
+</View>
 
     );
 }
